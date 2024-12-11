9c12b6de00ab6674cc8b4053dd7919fd1098b557
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2024-12-11 21:17:41 +0100

    Update mod.rs

--- src/day11/mod.rs
+++ src/day11/mod.rs
@@ -1,6 +1,8 @@
 mod game;
 mod parse;
 
+use std::error;
+
 use game::Game;
 use parse::parse_input;
 
@@ -11,7 +13,7 @@ const INPUT: &str = include_str!("input.txt");
 pub struct Part1;
 
 impl Part1 {
-    fn solve_input(input: &str) -> Result<u64, Box<dyn std::error::Error>> {
+    fn solve_input(input: &str) -> Result<u64, Box<dyn error::Error>> {
         let (_, numbers) = parse_input(input).map_err(|e| e.to_owned())?;
 
         let mut game = Game::new();
@@ -20,7 +22,7 @@ impl Part1 {
 }
 
 impl Puzzle for Part1 {
-    fn solve(&self) -> Result<u64, Box<dyn std::error::Error>> {
+    fn solve(&self) -> Result<u64, Box<dyn error::Error>> {
         Self::solve_input(INPUT)
     }
 }
@@ -28,7 +30,7 @@ impl Puzzle for Part1 {
 pub struct Part2;
 
 impl Part2 {
-    fn solve_input(input: &str) -> Result<u64, Box<dyn std::error::Error>> {
+    fn solve_input(input: &str) -> Result<u64, Box<dyn error::Error>> {
         let (_, numbers) = parse_input(input).map_err(|e| e.to_owned())?;
 
         let mut game = Game::new();
@@ -37,7 +39,7 @@ impl Part2 {
 }
 
 impl Puzzle for Part2 {
-    fn solve(&self) -> Result<u64, Box<dyn std::error::Error>> {
+    fn solve(&self) -> Result<u64, Box<dyn error::Error>> {
         Self::solve_input(INPUT)
     }
 }
