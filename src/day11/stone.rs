be1bca680e89d7b4310c83e228accf1046725a0d
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2024-12-12 00:03:34 +0100

    Update stone.rs

--- src/day11/stone.rs
+++ src/day11/stone.rs
@@ -1,36 +1,42 @@
 use std::error::Error;
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
-pub struct Stone(u64);
+pub struct Stone {
+    number: u64,
+}
 
 impl Stone {
-    pub fn new(stone: u64) -> Self {
-        Self(stone)
+    pub fn new(number: u64) -> Self {
+        Self { number }
     }
 
-    pub fn transform(&self) -> Result<Vec<Stone>, Box<dyn Error>> {
-        if self.0 == 0 {
-            return Ok(vec![Stone(1)]);
+    pub fn transform(&self) -> Result<Vec<Self>, Box<dyn Error>> {
+        if self.number == 0 {
+            return Ok(vec![Self::new(1)]);
         }
 
-        let stone_str = self.to_string();
+        let number_str = self.to_string();
 
-        if stone_str.len() % 2 == 0 {
-            let (first, second) = stone_str.split_at(stone_str.len() / 2);
-            Ok(vec![Stone(first.parse()?), Stone(second.parse()?)])
+        if number_str.len() % 2 == 0 {
+            let (first, second) = number_str.split_at(number_str.len() / 2);
+
+            Ok([first, second]
+                .iter()
+                .map(|s| s.parse().map(Self::new))
+                .collect::<Result<_, _>>()?)
         } else {
             Ok(vec![self.checked_mul(2024).ok_or("Overflow")?])
         }
     }
 
     fn checked_mul(&self, rhs: u64) -> Option<Self> {
-        self.0.checked_mul(rhs).map(Stone)
+        self.number.checked_mul(rhs).map(Self::new)
     }
 }
 
 impl ToString for Stone {
     fn to_string(&self) -> String {
-        self.0.to_string()
+        self.number.to_string()
     }
 }
 
@@ -40,8 +46,13 @@ mod tests {
 
     #[test]
     fn test_transform_stone() {
-        assert_eq!(Stone(0).transform().unwrap(), vec![Stone(1)]);
-        assert_eq!(Stone(1).transform().unwrap(), vec![Stone(2024)]);
-        assert_eq!(Stone(2024).transform().unwrap(), vec![Stone(20), Stone(24)]);
+        assert_eq!(Stone::new(0).transform().unwrap(), vec![Stone::new(1)]);
+
+        assert_eq!(Stone::new(1).transform().unwrap(), vec![Stone::new(2024)]);
+
+        assert_eq!(
+            Stone::new(2024).transform().unwrap(),
+            vec![Stone::new(20), Stone::new(24)]
+        );
     }
 }
