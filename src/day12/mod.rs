d83d732bd79c84f7d22c4247beabb54c0475db9f
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2024-12-12 14:46:57 +0100

    use nom for parsing

--- src/day12/mod.rs
+++ src/day12/mod.rs
@@ -2,6 +2,8 @@ mod parse;
 mod region;
 mod tile;
 
+use std::error::Error;
+
 use parse::parse_input;
 use region::Region;
 
@@ -12,36 +14,36 @@ const INPUT: &str = include_str!("input.txt");
 pub struct Part1;
 
 impl Part1 {
-    pub fn solve_input(input: &str) -> u64 {
-        let map = parse_input(input);
+    pub fn solve_input(input: &str) -> Result<u64, Box<dyn Error>> {
+        let (_, map) = parse_input(input).map_err(|e| e.to_owned())?;
 
         let regions = Region::find_regions(&map);
 
-        regions.iter().map(|r| r.price_v1()).sum()
+        Ok(regions.iter().map(|r| r.price_v1()).sum())
     }
 }
 
 impl Puzzle for Part1 {
-    fn solve(&self) -> Result<u64, Box<dyn std::error::Error>> {
-        Ok(Self::solve_input(INPUT))
+    fn solve(&self) -> Result<u64, Box<dyn Error>> {
+        Self::solve_input(INPUT)
     }
 }
 
 pub struct Part2;
 
 impl Part2 {
-    pub fn solve_input(input: &str) -> u64 {
-        let map = parse_input(input);
+    pub fn solve_input(input: &str) -> Result<u64, Box<dyn Error>> {
+        let (_, map) = parse_input(input).map_err(|e| e.to_owned())?;
 
         let regions = Region::find_regions(&map);
 
-        regions.iter().map(|r| r.price_v2()).sum()
+        Ok(regions.iter().map(|r| r.price_v2()).sum())
     }
 }
 
 impl Puzzle for Part2 {
-    fn solve(&self) -> Result<u64, Box<dyn std::error::Error>> {
-        Ok(Self::solve_input(INPUT))
+    fn solve(&self) -> Result<u64, Box<dyn Error>> {
+        Self::solve_input(INPUT)
     }
 }
 
@@ -57,17 +59,17 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(Part1::solve_input(TEST_INPUT), 140);
-        assert_eq!(Part1::solve_input(TEST_INPUT_2), 772);
-        assert_eq!(Part1::solve_input(TEST_INPUT_3), 1930);
+        assert_eq!(Part1::solve_input(TEST_INPUT).unwrap(), 140);
+        assert_eq!(Part1::solve_input(TEST_INPUT_2).unwrap(), 772);
+        assert_eq!(Part1::solve_input(TEST_INPUT_3).unwrap(), 1930);
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(Part2::solve_input(TEST_INPUT), 80);
-        assert_eq!(Part2::solve_input(TEST_INPUT_2), 436);
-        assert_eq!(Part2::solve_input(TEST_INPUT_3), 1206);
-        assert_eq!(Part2::solve_input(TEST_INPUT_4), 236);
-        assert_eq!(Part2::solve_input(TEST_INPUT_5), 368);
+        assert_eq!(Part2::solve_input(TEST_INPUT).unwrap(), 80);
+        assert_eq!(Part2::solve_input(TEST_INPUT_2).unwrap(), 436);
+        assert_eq!(Part2::solve_input(TEST_INPUT_3).unwrap(), 1206);
+        assert_eq!(Part2::solve_input(TEST_INPUT_4).unwrap(), 236);
+        assert_eq!(Part2::solve_input(TEST_INPUT_5).unwrap(), 368);
     }
 }
--- src/day12/parse.rs
+++ src/day12/parse.rs
@@ -1,17 +1,37 @@
 use std::collections::HashMap;
 
+use nom::{
+    character::{
+        complete::{newline, satisfy},
+        is_alphabetic,
+    },
+    combinator::map,
+    multi::{many0, separated_list0},
+    IResult,
+};
+
 use crate::util::Point;
 
 use super::tile::Tile;
 
-pub fn parse_input(input: &str) -> HashMap<Point, Tile> {
-    input
-        .lines()
-        .enumerate()
-        .flat_map(|(y, line)| {
-            line.chars()
+pub fn parse_input(input: &str) -> IResult<&str, HashMap<Point, Tile>> {
+    map(
+        separated_list0(newline, many0(map(parse_one_alpha, Tile::new))),
+        |lines| {
+            lines
+                .into_iter()
                 .enumerate()
-                .map(move |(x, c)| (Point(x as i64, y as i64), Tile::new(c)))
-        })
-        .collect()
+                .flat_map(|(y, tiles)| {
+                    tiles
+                        .into_iter()
+                        .enumerate()
+                        .map(move |(x, tile)| (Point(x as i64, y as i64), tile))
+                })
+                .collect()
+        },
+    )(input)
+}
+
+fn parse_one_alpha(input: &str) -> nom::IResult<&str, char> {
+    satisfy(|c| is_alphabetic(c as u8))(input)
 }
