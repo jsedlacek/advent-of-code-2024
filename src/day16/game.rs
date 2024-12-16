59df522d95b90ff946b256dbc65882d9d4673cc9
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2024-12-16 13:21:40 +0100

    day 16 part 2

--- src/day16/game.rs
+++ src/day16/game.rs
@@ -1,4 +1,4 @@
-use std::collections::{BinaryHeap, HashSet};
+use std::collections::{BinaryHeap, HashMap, HashSet};
 
 use crate::util::{Direction, Point};
 
@@ -8,8 +8,8 @@ pub struct Game {
     exit: Point,
 }
 
-#[derive(Debug, Hash, Clone, PartialEq, Eq)]
-struct QueueItem(Point, Direction, u64);
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct QueueItem(Point, Direction, u64, HashSet<Point>);
 
 impl Ord for QueueItem {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
@@ -32,23 +32,54 @@ impl Game {
         }
     }
 
-    pub fn play(&self) -> u64 {
-        let mut visited = HashSet::new();
+    pub fn part1(&self) -> u64 {
+        let (score, _) = self.best_paths();
+        score
+    }
+
+    pub fn part2(&self) -> u64 {
+        let (_, points) = self.best_paths();
+
+        points.len() as u64
+    }
+
+    pub fn best_paths(&self) -> (u64, HashSet<Point>) {
+        let mut visited = HashMap::new();
 
         let mut queue = BinaryHeap::new();
-        queue.push(QueueItem(self.start, Direction::Right, 0));
+        queue.push(QueueItem(self.start, Direction::Right, 0, HashSet::new()));
+
+        let mut min_score = None;
+        let mut best_path_positions = HashSet::new();
 
         loop {
-            let QueueItem(pos, dir, score) = queue.pop().expect("No path found");
+            let QueueItem(pos, dir, score, path) = queue.pop().expect("No path found");
 
-            if visited.contains(&(pos, dir)) {
-                continue;
+            if let Some(&visited_score) = visited.get(&(pos, dir)) {
+                if visited_score < score {
+                    continue;
+                }
             }
 
-            visited.insert((pos, dir));
+            visited.insert((pos, dir), score);
+
+            if let Some(min_score) = min_score {
+                if score > min_score {
+                    break;
+                }
+            }
+
+            let mut path = path.clone();
+            path.insert(pos);
 
             if pos == self.exit {
-                return score;
+                if min_score.is_none() {
+                    min_score = Some(score);
+                }
+
+                for &point in &path {
+                    best_path_positions.insert(point);
+                }
             }
 
             for (next_pos, next_dir, next_score) in [
@@ -57,10 +88,12 @@ impl Game {
                 (pos, dir.rotate_clockwise(), score + 1000),
             ] {
                 if !self.walls.contains(&next_pos) {
-                    queue.push(QueueItem(next_pos, next_dir, next_score));
+                    queue.push(QueueItem(next_pos, next_dir, next_score, path.clone()));
                 }
             }
         }
+
+        return (min_score.unwrap(), best_path_positions);
     }
 }
 
@@ -72,30 +105,36 @@ mod tests {
 
     const TEST_INPUT: &str = include_str!("test-input.txt");
     const TEST_INPUT_2: &str = include_str!("test-input-2.txt");
-    const INPUT: &str = include_str!("input.txt");
 
     #[test]
-    fn test_play() {
+    fn test_part1() {
         let (_, game) = parse_input(TEST_INPUT).unwrap();
 
-        assert_eq!(game.play(), 7036);
+        assert_eq!(game.part1(), 7036);
 
         let (_, game) = parse_input(TEST_INPUT_2).unwrap();
 
-        assert_eq!(game.play(), 11048);
+        assert_eq!(game.part1(), 11048);
+    }
 
-        let (_, game) = parse_input(INPUT).unwrap();
+    #[test]
+    fn test_part2() {
+        let (_, game) = parse_input(TEST_INPUT).unwrap();
 
-        assert_eq!(game.play(), 134588);
+        assert_eq!(game.part2(), 45);
+
+        let (_, game) = parse_input(TEST_INPUT_2).unwrap();
+
+        assert_eq!(game.part2(), 64);
     }
 
     #[test]
     fn test_binary_heap() {
         let mut queue = BinaryHeap::new();
-        queue.push(QueueItem(Point(0, 0), Direction::Right, 0));
-        queue.push(QueueItem(Point(1, 0), Direction::Right, 2));
-        queue.push(QueueItem(Point(2, 0), Direction::Right, 1));
-        queue.push(QueueItem(Point(3, 0), Direction::Right, 1));
+        queue.push(QueueItem(Point(0, 0), Direction::Right, 0, HashSet::new()));
+        queue.push(QueueItem(Point(1, 0), Direction::Right, 2, HashSet::new()));
+        queue.push(QueueItem(Point(2, 0), Direction::Right, 1, HashSet::new()));
+        queue.push(QueueItem(Point(3, 0), Direction::Right, 1, HashSet::new()));
 
         assert_eq!(queue.pop().unwrap().2, 0);
         assert_eq!(queue.pop().unwrap().2, 1);
--- src/day16/mod.rs
+++ src/day16/mod.rs
@@ -13,11 +13,27 @@ impl Part1 {
     fn solve_input(input: &str) -> Result<u64, Box<dyn std::error::Error>> {
         let (_, game) = parse_input(input).map_err(|e| e.to_owned())?;
 
-        Ok(game.play())
+        Ok(game.part1())
     }
 }
 
 impl Puzzle for Part1 {
+    fn solve(&self) -> Result<u64, Box<dyn std::error::Error>> {
+        Self::solve_input(INPUT)
+    }
+}
+
+pub struct Part2;
+
+impl Part2 {
+    fn solve_input(input: &str) -> Result<u64, Box<dyn std::error::Error>> {
+        let (_, game) = parse_input(input).map_err(|e| e.to_owned())?;
+
+        Ok(game.part2())
+    }
+}
+
+impl Puzzle for Part2 {
     fn solve(&self) -> Result<u64, Box<dyn std::error::Error>> {
         Self::solve_input(INPUT)
     }
